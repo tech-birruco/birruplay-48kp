73504caad4b0ea3d702898646ed54a276543de44
dinastran <dinastran@gmail.com>
2024-12-21 16:36:00 +0700

    ganti hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -26,15 +26,13 @@ export default function Hero() {
 
           <div className="my-8"></div>
 
-          <div className="inline-flex items-center gap-2 bg-primary-100 text-primary-800 px-4 py-2 rounded-full mb-8">
-            <Sparkles className="w-5 h-5" />
-            <span className="text-sm font-medium">Percepat Kemampuan Anak Menyampaikan Keinginan</span>
-          </div>
-
           <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-6 leading-tight">
-          Berbasis kurikulum international, bantu anak menyusun kalimat sederhana,{' '}
-            <span className="text-primary-600">menyampaikan keinginannya,</span> dan membangun koneksi lebih dekat dengan Ayah Bunda
+          Percepat Kemampuan Anak{' '} <br />
+            <span className="text-primary-600">Menyampaikan Keinginan</span>
           </h1>
+          <p className="text-xl text-gray-700 mb-8">
+          Berbasis kurikulum international, bantu anak menyusun kalimat sederhana, menyampaikan keinginannya, dan membangun koneksi lebih dekat dengan Ayah Bunda
+          </p>
           <CTAButton className="bg-primary-500 text-white px-10 py-4 rounded-full text-lg font-semibold hover:bg-primary-600">
           Mulai Perjalanan Bicara Anak Sekarang!
           </CTAButton>
